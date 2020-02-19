2e23d8252a8ae1390aae3577cba7b1654be34d02
Elie NEHME <elie29@gmail.com>
2020-02-19 19:00:36 +0100

    Test docker only

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
-kubectl apply -f k8s
+#kubectl apply -f k8s
 
-kubectl set image deployments/server-deployment server=elie29/server:$SHA
-kubectl set image deployments/client-deployment client=elie29/client:$SHA
-kubectl set image deployments/worker-deployment worker=elie29/worker:$SHA
+#kubectl set image deployments/server-deployment server=elie29/server:$SHA
+#kubectl set image deployments/client-deployment client=elie29/client:$SHA
+#kubectl set image deployments/worker-deployment worker=elie29/worker:$SHA
