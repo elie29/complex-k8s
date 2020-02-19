0534e3aa0cbcc6d8fcbff94630637bb529bde875
Elie NEHME <elie29@gmail.com>
2020-02-19 17:52:13 +0100

    Test gcp deploy on success

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+kubectl apply -f k8s
+
+kubectl set image deployments/server-deployment server=elie29/server:$SHA
+kubectl set image deployments/client-deployment client=elie29/client:$SHA
+kubectl set image deployments/worker-deployment worker=elie29/worker:$SHA
